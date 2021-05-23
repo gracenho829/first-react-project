ad2045df803dd1f55174a6b39531ed83fdf0f143
Grace Nho <gracenho829@gmail.com>
2021-05-23 12:03:33 +0900

    expenseform

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import Expenses from "./components/Expenses/Expenses"
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () =>{
   const expenses = [
@@ -27,6 +28,7 @@ const App = () =>{
   return (
 
     <div>
+      <NewExpense/>
       <h2>Let's Get Started</h2>
       <Expenses expenses={expenses}/>
     </div>
